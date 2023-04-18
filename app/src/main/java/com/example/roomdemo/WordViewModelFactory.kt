dd98e558e70936e150b6e80e74dc9ae2dfe1836b
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-19 00:00:09 +0200

    database repository and view model added

---
+++ app/src/main/java/com/example/roomdemo/WordViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.roomdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.roomdemo.db.WordRepository
+
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
